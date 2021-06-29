ef0a13116957ac2fb7a4bce019b7c000bf608909
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2021-06-29 17:44:38 +0430

    removed BroadcastServiceProvider, broadcast config & broadcast channels routes

--- config/app.php
+++ config/app.php
@@ -139,7 +139,6 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
@@ -169,7 +168,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
